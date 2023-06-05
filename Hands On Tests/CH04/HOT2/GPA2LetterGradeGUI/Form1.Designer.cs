9f11723344f98d53637f87f4174671ead32c57c6
mayaWernerRanken <jemell_werner@insideranken.org>
2023-06-05 03:52:05 -0500

    HOT2

--- Hands On Tests/CH04/HOT2/GPA2LetterGradeGUI/Form1.Designer.cs
+++ Hands On Tests/CH04/HOT2/GPA2LetterGradeGUI/Form1.Designer.cs
@@ -28,18 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnCalculate = new Button();
+            btnClear = new Button();
+            btnExit = new Button();
+            lblResult = new Label();
+            txtNumGrade = new TextBox();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Font = new Font("Segoe UI Semilight", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCalculate.Location = new Point(137, 285);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(194, 78);
+            btnCalculate.TabIndex = 0;
+            btnCalculate.Text = "Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new Font("Segoe UI Semilight", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            btnClear.Location = new Point(417, 285);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(194, 78);
+            btnClear.TabIndex = 1;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Font = new Font("Segoe UI Semilight", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            btnExit.Location = new Point(697, 285);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(194, 78);
+            btnExit.TabIndex = 2;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // lblResult
+            // 
+            lblResult.Anchor = AnchorStyles.None;
+            lblResult.AutoSize = true;
+            lblResult.BackColor = Color.FromArgb(192, 255, 192);
+            lblResult.Font = new Font("Segoe UI Semilight", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            lblResult.Location = new Point(404, 425);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(0, 31);
+            lblResult.TabIndex = 3;
+            // 
+            // txtNumGrade
+            // 
+            txtNumGrade.Font = new Font("Segoe UI Semilight", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            txtNumGrade.Location = new Point(410, 53);
+            txtNumGrade.Name = "txtNumGrade";
+            txtNumGrade.Size = new Size(209, 38);
+            txtNumGrade.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(192, 255, 192);
+            label1.Font = new Font("Segoe UI Semilight", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(137, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(170, 31);
+            label1.TabIndex = 5;
+            label1.Text = "Numeric Grade:";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AcceptButton = btnCalculate;
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 366);
+            BackColor = Color.FromArgb(255, 192, 128);
+            CancelButton = btnClear;
+            ClientSize = new Size(1029, 488);
+            Controls.Add(label1);
+            Controls.Add(txtNumGrade);
+            Controls.Add(lblResult);
+            Controls.Add(btnExit);
+            Controls.Add(btnClear);
+            Controls.Add(btnCalculate);
+            Margin = new Padding(4);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCalculate;
+        private Button btnClear;
+        private Button btnExit;
+        private Label lblResult;
+        private TextBox txtNumGrade;
+        private Label label1;
     }
 }
--- Hands On Tests/CH04/HOT2/GPA2LetterGradeGUI/Form1.cs
+++ Hands On Tests/CH04/HOT2/GPA2LetterGradeGUI/Form1.cs
@@ -6,5 +6,42 @@ namespace GPA2LetterGradeGUI
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double numGrade = Convert.ToDouble(txtNumGrade.Text);
+
+            if (numGrade > 3.2 && numGrade <= 4.0)
+            {
+                lblResult.Text = "Your letter grade is: A";
+            }
+            else if (numGrade > 2.5 && numGrade <= 3.2)
+            {
+                lblResult.Text = "Your letter grade is: B";
+            }
+            else if (numGrade > 1.5 && numGrade <= 2.5)
+            {
+                lblResult.Text = "Your letter grade is: C";
+            }
+            else if (numGrade > 0.0 && numGrade <= 1.5)
+            {
+                lblResult.Text = "Your letter grade is: D";
+            }
+            else if (numGrade == 0.0)
+            {
+                lblResult.Text = "Your letter grade is: F";
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = string.Empty;
+            txtNumGrade.Text = string.Empty;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
