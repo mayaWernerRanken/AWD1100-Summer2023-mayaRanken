b2e89b87555eae01d7793b6cd00df5e6d7f209c0
mayaWernerRanken <jemell_werner@insideranken.org>
2023-08-02 21:57:19 -0500

    CH17 WIP

--- ExtraExercises/ExtraStarts/ch17/InventoryMaintenanceText/InventoryMaintenance/InventoryDB.cs
+++ ExtraExercises/ExtraStarts/ch17/InventoryMaintenanceText/InventoryMaintenance/InventoryDB.cs
@@ -2,7 +2,7 @@ namespace InventoryMaintenance
 {
 	public static class InventoryDB
 	{
-		private const string Path = @"..\..\..\InventoryItems.txt";
+		private const string Path = @"C:\Users\Maya\Documents\GitHub\AWD1100-Summer2023-mayaRanken\ExtraExercises\ExtraStarts\ch17\InventoryMaintenanceText\InventoryMaintenance\InventoryItems.txt";
 
         public static List<InventoryItem> GetItems()
 		{
@@ -10,6 +10,8 @@ namespace InventoryMaintenance
             List<InventoryItem> items = new();
 
             // Add code here to read the text file into the List<InventoryItem> object.
+            FileStream fileStream = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.Read);
+
 
             return items;
 		}
