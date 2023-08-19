be6d088cf4287b4c34e49fe586d298d000209640
mayaWernerRanken <jemell_werner@insideranken.org>
2023-08-18 22:41:35 -0500

    WIP

--- Projects/Files/LB01/EX2/Program.cs
+++ Projects/Files/LB01/EX2/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace EX2
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
